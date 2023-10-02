ab9a743595292aa8b0c458f035df03057c0fb5c0
Jonas B. Franco <jonasbfranco@hotmail.com>
2023-10-02 16:34:53 -0300

    add resources in register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -30,12 +30,12 @@
                         <div class="mb-3">
                             <label for="name" class="form-label">Name</label>
                             <input type="text" name="name" class="form-control" id="name"
-                                placeholder="John Doe" required>
+                                placeholder="John Doe" value="{{ @old('name') }}" required>
                         </div>
                         <div class="mb-3">
                             <label for="email" class="form-label">Email address</label>
                             <input type="email" name="email" class="form-control" id="email"
-                                placeholder="name@example.com" required>
+                                placeholder="name@example.com" value="{{ @old('email') }}" required>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
